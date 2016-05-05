fcfcea23a6acd1b6616e4c1ed8f97e1f7556635f
Graham Kaemmer <graham.kaemmer@yale.edu>
2016-05-04 23:11:19 -0400

    Fix path resolution

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 app.listen(process.env.PORT || 3456);
